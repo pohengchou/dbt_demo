eacee4e96299c6d49ddbd7c3bb17d7b1973b3794
pohengchou <poheng131@gmail.com>
2025-09-21 13:00:08 +0000

    stg_weather.sql完成

--- models/staging/stg_ubike_staging__raw_weather_data.sql
+++ models/staging/stg_ubike_staging__raw_weather_data.sql
@@ -1,26 +1,56 @@
 {{ config(materialized='view') }}
 
-WITH raw AS (
-  SELECT *
-  FROM {{ source('ubike_staging', 'raw_weather_data') }}
+WITH source AS (
+    SELECT *
+    FROM {{ source('ubike_staging', 'raw_weather_data') }}
+),
+
+flattened AS (
+    SELECT
+        StationId AS station_id,
+        StationName AS station_name,
+        
+        GeoInfo.TownName AS town_name,
+        GeoInfo.CountyName AS county_name,
+        CAST(GeoInfo.TownCode AS INT64) AS town_code,   
+        CAST(GeoInfo.CountyCode AS INT64) AS county_code, 
+        CAST(GeoInfo.StationAltitude AS FLOAT64) AS station_altitude,
+        
+        coordinate.CoordinateName AS coordinate_name,
+        CAST(coordinate.StationLatitude AS FLOAT64) AS latitude,
+        CAST(coordinate.StationLongitude AS FLOAT64) AS longitude,
+        
+        PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S%z', ObsTime.DateTime) AS observation_timestamp,
+        
+        WeatherElement.VisibilityDescription AS visibility_description,
+        CAST(WeatherElement.AirPressure AS FLOAT64) AS air_pressure,
+        CAST(WeatherElement.AirTemperature AS FLOAT64) AS air_temperature,
+        CAST(WeatherElement.RelativeHumidity AS FLOAT64) AS relative_humidity,
+        CAST(WeatherElement.WindSpeed AS FLOAT64) AS wind_speed,
+        CAST(WeatherElement.WindDirection AS FLOAT64) AS wind_direction,
+        CAST(WeatherElement.UVIndex AS INT64) AS uv_index,
+        CAST(WeatherElement.SunshineDuration AS FLOAT64) AS sunshine_duration,
+        CAST(WeatherElement.Precipitation AS FLOAT64) AS precipitation,
+        WeatherElement.Weather AS weather_description,
+        
+        CAST(WeatherElement.DailyExtreme.DailyLow.TemperatureInfo.AirTemperature AS FLOAT64) AS daily_low_temp,
+        PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S%z', WeatherElement.DailyExtreme.DailyLow.TemperatureInfo.Occurred_at.DateTime) AS daily_low_temp_at,
+        
+        CAST(WeatherElement.DailyExtreme.DailyHigh.TemperatureInfo.AirTemperature AS FLOAT64) AS daily_high_temp,
+        PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S%z', WeatherElement.DailyExtreme.DailyHigh.TemperatureInfo.Occurred_at.DateTime) AS daily_high_temp_at,
+        
+        CAST(WeatherElement.GustInfo.PeakGustSpeed AS FLOAT64) AS peak_gust_speed,
+        CAST(WeatherElement.GustInfo.Occurred_at.WindDirection AS FLOAT64) AS gust_direction,
+        PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S%z', WeatherElement.GustInfo.Occurred_at.DateTime) AS gust_occurred_at,
+        
+        CAST(WeatherElement.Max10MinAverage.WindSpeed AS FLOAT64) AS max_10min_avg_wind_speed,
+        CAST(WeatherElement.Max10MinAverage.Occurred_at.WindDirection AS FLOAT64) AS max_10min_avg_wind_direction,
+        PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S%z', WeatherElement.Max10MinAverage.Occurred_at.DateTime) AS max_10min_avg_wind_at,
+        
+    FROM
+        source,
+        UNNEST(source.GeoInfo.Coordinates) AS coordinate
 )
 
-SELECT
-  -- 時間處理
-  DATETIME(TIMESTAMP(raw.ObsTime.DateTime), "Asia/Taipei") AS obs_time_local,
-
-  -- 基本地理資訊
-  raw.GeoInfo.CountyName AS county_name,
-  raw.GeoInfo.TownName AS town_name,
-  CAST(raw.GeoInfo.Coordinates[OFFSET(0)].StationLatitude AS FLOAT64) AS lat,
-  CAST(raw.GeoInfo.Coordinates[OFFSET(0)].StationLongitude AS FLOAT64) AS lng,
-
-  -- 天氣指標（-99 → NULL）
-  NULLIF(raw.WeatherElement.AirTemperature, -99) AS air_temperature,
-  NULLIF(raw.WeatherElement.RelativeHumidity, -99) AS humidity,
-  NULLIF(raw.WeatherElement.WindSpeed, -99) AS wind_speed,
-  raw.WeatherElement.Weather AS weather,
-  NULLIF(raw.WeatherElement.Precipitation, -99) AS precipitation,
-  NULLIF(raw.WeatherElement.AirPressure, -99) AS air_pressure
-FROM raw
-WHERE raw.ObsTime.DateTime IS NOT NULL
+SELECT * FROM flattened
+WHERE coordinate_name = 'WGS84'
