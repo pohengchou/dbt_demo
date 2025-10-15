2616f827fe732aeab098cfe2e33a68b9eadd32b7
pohengchou <poheng131@gmail.com>
2025-10-15 21:10:06 +0800

    incremental_strategy 調整成'merge' 並且增加時間的cluster 來優化效能成本

--- models/marts/fact_weather.sql
+++ models/marts/fact_weather.sql
@@ -1,12 +1,13 @@
 {{ config(
     materialized='incremental',
-    incremental_strategy='insert_overwrite',
+    incremental_strategy='merge ',
+    unique_key=['observation_timestamp', 'weather_station_id_key'],
     partition_by={
       "field": "observation_timestamp",
       "data_type": "timestamp",
       "granularity": "day"
     },
-    cluster_by=['weather_station_id_key']
+    cluster_by=['weather_station_id_key', 'observation_timestamp']
 ) }}
 
 SELECT
